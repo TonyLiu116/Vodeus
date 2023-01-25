a7cc3ee785134864dd07912f4e7eeda4613963c0
promarat <ace1998116@gmail.com>
2023-01-25 07:53:14 +0300

    s

--- screens/PhoneNumberLogin/PhoneRegisterScreen.js
+++ screens/PhoneNumberLogin/PhoneRegisterScreen.js
@@ -218,7 +218,7 @@ const PhoneRegisterScreen = (props) => {
         try {
             const appleAuthRequestResponse = await appleAuth.performRequest({
                 requestedOperation: appleAuth.Operation.LOGIN,
-                requestedScopes: [appleAuth.Scope.FULL_NAME,appleAuth.Scope.EMAIL]
+                requestedScopes: [appleAuth.Scope.EMAIL]
             })
             const response = ({
                 nonce,
