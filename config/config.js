82de04dfb6f2abb69f8fe5eab7024d0c43c63894
promarat <ace1998116@gmail.com>
2023-02-01 23:35:49 +0300

    s

--- config/config.js
+++ config/config.js
@@ -2,12 +2,12 @@ import { Platform, Dimensions } from 'react-native';
 export const windowWidth = Dimensions.get('window').width;
 export const windowHeight = Dimensions.get('window').height;
 
-//export const API_URL = 'https://api.vocco.ai';
-export const API_URL = 'https://test.vocco.ai';
+export const API_URL = 'https://api.vocco.ai';
+//export const API_URL = 'https://test.vocco.ai';
 //export const API_URL = 'http://10.0.2.2:80';
 // export const SOCKET_URL = 'https://realservice-kqnrsfqveq-od.a.run.app';
-//export const SOCKET_URL = 'https://sok.vocco.ai';
-export const SOCKET_URL = 'https://tsok.vocco.ai';
+export const SOCKET_URL = 'https://sok.vocco.ai';
+//export const SOCKET_URL = 'https://tsok.vocco.ai';
 export const ACCESSTOKEN_KEY = "@VoccoAT:2021";
 export const REFRESHTOKEN_KEY = "@VoccoRT:2021";
 export const TUTORIAL_CHECK = "tutorial";
