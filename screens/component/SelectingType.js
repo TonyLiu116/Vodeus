190aa8b5b107d7b86e4a61b4edc6a9b48b644c5c
promarat <ace1998116@gmail.com>
2023-04-17 12:34:14 +0300

    s

--- screens/component/SelectingType.js
+++ screens/component/SelectingType.js
@@ -1,53 +1,149 @@
-import React from "react";
-import { TitleText } from "./TitleText";
-import { View, TouchableOpacity } from "react-native";
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Modal, Pressable, TouchableOpacity, View } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import { SvgXml } from 'react-native-svg';
+import { useSelector } from 'react-redux';
+import whiteAlphaSvg from '../../assets/common/white_alpha.svg';
+import whiteFriendSvg from '../../assets/common/white_friend.svg';
+import whiteRoomSvg from '../../assets/common/white_room.svg';
+import { windowWidth } from '../../config/config';
 import '../../language/i18n';
-import { DescriptionText } from "./DescriptionText";
+import { styles } from '../style/Common';
+import { DescriptionText } from './DescriptionText';
 
-export const BlockList = ({
-  marginTop,
-  blockName,
-  items,
-  onLoadHistory = () => { },
-  onClear = ()=>{}
+export const SelectingType = ({
+  props,
+  onNewRoom = () => { },
+  onNewPost = () => { },
+  onInvitePeople = () => { },
+  onCloseModal = () => { }
 }) => {
 
   const { t, i18n } = useTranslation();
+  const [showModal, setShowModal] = useState(true);
+
+  const user = useSelector(state => state.user.user);
+
+  const closeModal = () => {
+    onCloseModal();
+    setShowModal(false);
+  }
 
   return (
-    <View style={{ marginTop }}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-        <TitleText
-          text={blockName}
-          color='#281E30'
-          fontSize={15}
-        />
-        <TouchableOpacity onPress={()=>onClear()}>
-          <TitleText
-            text={t('Clear')}
-            marginRight={16}
-            fontSize={15}
-            fontFamily="SFProDisplay-Regular"
-            color='rgba(38, 52, 73, 0.85)'
-          />
-        </TouchableOpacity>
-      </View>
-      <View style={{ width: '100%', height: 1, backgroundColor: '#F0F4FC', marginTop: 9, marginBottom: 18 }}></View>
-      {items.map((item, index) => {
-        return (
-          <TouchableOpacity onPress={() => onLoadHistory(item)} key={item.title + index.toString()}>
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={showModal}
+      onRequestClose={() => {
+        closeModal();
+      }}
+    >
+      <Pressable onPressOut={closeModal} style={[styles.swipeModal, { justifyContent: 'flex-end' }]}>
+        <LinearGradient
+          style={
+            {
+              width: windowWidth,
+              borderRadius: 17,
+              alignItems: 'center',
+            }
+          }
+          start={{ x: 0, y: 0 }} end={{ x: 0, y: 1 }}
+          locations={[0, 1]}
+          colors={['#6051AD', '#423582']}
+        >
+          <View style={{
+            width: '100%',
+            alignItems: 'center',
+            marginTop: 17,
+            marginBottom: 28
+          }}>
+            <View style={{
+              width: 75,
+              height: 5,
+              backgroundColor: '#443688',
+              borderRadius: 4
+            }}>
+
+            </View>
+          </View>
+          <TouchableOpacity
+            style={{
+              width: windowWidth - 32,
+              height: 73,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              borderRadius: 12,
+              backgroundColor: '#443688',
+              alignItems: 'center',
+              paddingLeft: 24,
+              paddingRight: 18
+            }}
+            onPress={() => onNewRoom()}
+          >
             <DescriptionText
-              text={item}
-              key={index + 'blocklist'}
-              fontSize={15}
-              fontFamily='SFProDisplay-Regular'
-              color='#281E30'
-              marginBottom={20}
+              text={t("New Room")}
+              color='#FFF'
+              fontSize={20}
+              lineHeight={26}
+            />
+            <SvgXml
+              xml={whiteRoomSvg}
             />
           </TouchableOpacity>
-        )
-      })}
-    </View>
+          <TouchableOpacity
+            style={{
+              width: windowWidth - 32,
+              height: 73,
+              marginTop: 7,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              borderRadius: 12,
+              backgroundColor: '#443688',
+              alignItems: 'center',
+              paddingLeft: 24,
+              paddingRight: 18
+            }}
+            onPress={() => onNewPost()}
+          >
+            <DescriptionText
+              text={t("New Post")}
+              color='#FFF'
+              fontSize={20}
+              lineHeight={26}
+            />
+            <SvgXml
+              xml={whiteAlphaSvg}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              width: windowWidth - 32,
+              height: 73,
+              marginTop: 7,
+              marginBottom: 25,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              borderRadius: 12,
+              backgroundColor: '#443688',
+              alignItems: 'center',
+              paddingLeft: 24,
+              paddingRight: 18
+            }}
+            onPress={() => onInvitePeople()}
+          >
+            <DescriptionText
+              text={t("Invite People")}
+              color='#FFF'
+              fontSize={20}
+              lineHeight={26}
+            />
+            <SvgXml
+              xml={whiteFriendSvg}
+            />
+          </TouchableOpacity>
+        </LinearGradient>
+      </Pressable>
+    </Modal>
   );
 };
